d89a503f07437c4c55f75fef0e23a6672c2692a4
pacokwon <haechank@gmail.com>
2021-03-11 10:10:47 +0000

    feat(cors): origin check in cors middleware

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -2,10 +2,17 @@ import { RequestHandler } from 'express';
 
 // cors middleware for development
 export const corsMiddleware: RequestHandler = (req, res, next) => {
+  const originRegex = /^https?:\/\/localhost(?::[0-9]{1,5})?$/;
+  const origin = req.get('origin') ?? '';
+  if (!originRegex.test(origin)) {
+    res.status(400).send("Origin not suited for this auth server's purpose!");
+    return;
+  }
+
   res.set('Access-Control-Allow-Origin', req.get('origin'));
   res.set('Access-Control-Allow-Credentials', 'true');
   res.set('Access-Control-Allow-Methods', '*');
-  res.set('Access-Control-Allow-Headers', 'x-access-token');
+  res.set('Access-Control-Allow-Headers', 'Content-Type,x-access-token');
 
   if (req.method === 'OPTIONS') res.sendStatus(200);
   else next();
